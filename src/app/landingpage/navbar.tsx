eb4520f0ec9b898b3bd29d109362e3010b96d887
bydewanto <bydewanto@gmail.com>
2024-10-08 15:37:19 +0700

    basic landing page, font still unset

---
+++ src/app/landingpage/navbar.tsx
@@ -0,0 +1,30 @@
+const navbar = () => {
+    return (
+    <nav className="bg-white border-gray-200">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-10 p-4">
+        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src="/image/benkod_logo.webp" className="h-10" alt="Bengkel Koding Logo" />
+        </a>
+        <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+            <span className="sr-only">Open main menu</span>
+            <svg className="w-5 h-5" aria-hidden="true" fill="none" viewBox="0 0 17 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+            </svg>
+        </button>
+        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <ul className="justify-center items-center font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
+            <li>
+              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Tentang Kami</a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Tutorial</a>
+            </li>
+            <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Login</button>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    )
+};
+
+export default navbar;
---
+++ src/app/landingpage/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import NavBar from "./navbar";
+import BodLad from "./body";
+
+export default function Home() {
+  return (
+    <div>
+      <NavBar />
+
+      <BodLad />
+    </div>
+  )
+};
