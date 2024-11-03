f300c9722ce88974d6dcb1aa11d8ab309f687875
thisSurya <suryamichael09@gmail.com>
2024-11-03 19:40:49 +0700

    color-palette tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from 'tailwindcss'
-
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -12,9 +11,77 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        
       },
+      colors:{
+        gray: {
+          25: '#FCFCFD',
+          50: '#F9FAFB',
+          100: '#F2F4F7',
+          200: '#E4E7EC',
+          300: '#D0D5DD',
+          400: '#98A2B3',
+          500: '#667085',
+          600: '#475467',
+          700: '#344054',
+          800: '#344054',
+          900: '#344054',
+        },
+        primary: {
+          25: '#FBFCFE',
+          50: '#F7F8FD',
+          100: '#EEF2FB',
+          200: '#DDE4F8',
+          300: '#C4D0F2',
+          400: '#9FB2EA',
+          500: '#819BE4',
+          600: '#5376DA',
+          700: '#365ED4',
+          800: '#264AB0',
+          900: '#1E3A8B'
+        },
+        red: {
+          25: '#FFFBFA',
+          50: '#FEF3F2',
+          100: '#FEE4E2',
+          200: '#FECDCA',
+          300: '#FDA29B',
+          400: '#F97066',
+          500: '#F04438', 
+          600: '#D92D20',
+          700: '#B42318',
+          800: '#912018',
+          900: '#7A271A'
+        },
+        yellow: {
+          25: '#FFFCF5',
+          50: '#FFFAEB',
+          100: '#FEF0C7',
+          200: '#FEDF89',
+          300: '#FEC84B',
+          400: '#FDB022',
+          500: '#F79009',
+          600: '#DC6803',
+          700: '#B54708',
+          800: '#93370D',
+          900: '#7A2E0E',
+        },
+        green: {
+          25: '#F6FEF9',
+          50: '#ECFDF3',
+          100: '#D1FADF',
+          200: '#A6F4C5',
+          300: '#6CE9A6',
+          400: '#32D583',
+          500: '#12B76A',
+          600: '#039855',
+          700: '#027A48',
+          800: '#05603A',
+          900: '#054F31',
+        },
+      }
+
     },
   },
-  plugins: [],
 }
 export default config
